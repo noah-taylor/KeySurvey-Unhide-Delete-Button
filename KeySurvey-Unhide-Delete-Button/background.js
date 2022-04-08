2e8bf6a1c6c4ab871e35cae5ec4a53a6e05264ec
noah-taylor <77985547+noah-taylor@users.noreply.github.com>
2022-04-08 15:42:41 -0700

    Create background.js

---
+++ KeySurvey-Unhide-Delete-Button/background.js
@@ -0,0 +1,21 @@
+function showDeleteButton() {
+  console.log('started show delete button function');
+  var buttons = document.querySelectorAll('button');
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].innerText.match(/delete/gi)) {
+      buttons[i].style.display = 'block';
+    }
+  }
+}
+
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    if (tab.url.match(/survey2\.beyondfeedback\.com\/reportmodule/gi)) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: showDeleteButton
+      });
+    }
+  }
+});
